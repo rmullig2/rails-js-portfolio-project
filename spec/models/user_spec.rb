f1b208d4747aed43244c39981071a1c2f2ee20c5
Ray Mulligan <rmullig2@optonline.net>
2017-02-09 16:11:56 -0500

    Created spec tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { @user = User.new(email: 'test_user@yahoo.com', password: "password123") }
+
+  subject { @user }
+
+  it { should respond_to(:email) }
+
+  it "#email returns a string" do
+    expect(@user.email).to match 'test_user@yahoo.com'
+  end
+
+  it "#role is 'user' by default" do
+    expect(@user.role).to eq "user"
+  end
+
+  it "#role can be set to :admin" do
+    @user.role = :admin
+    expect(@user.role).to eq "admin"
+  end
+
 end
