7a41bab9ce0be92ef3008cd64a3d0833292820ed
Ray Mulligan <rmullig2@optonline.net>
2017-02-09 22:56:22 -0500

    create user associations, update spec.md

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   # Removed the following default modules: :trackable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  has_many :reviews
+  has_many :books, through: :reviews
 end
