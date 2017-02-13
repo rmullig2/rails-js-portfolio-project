a28cfc8e3acfda935e250aebdec65081f7d53ee5
Ray Mulligan <rmullig2@optonline.net>
2017-02-12 20:35:04 -0500

    Add unique user/book validation to review and test

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
   validates :rating, :summary, :user, :book, presence: true
+  validates_uniqueness_of :user, scope: :book
   scope :user_review, ->(u) { where("user_id = ?", u.id) }
   scope :book_review, ->(b) { where("book_id = ?", b.id) }
 end
