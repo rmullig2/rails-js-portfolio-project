e92f31be75f40a42ba07f83dbe7655781042f746
Ray Mulligan <rmullig2@optonline.net>
2017-02-13 12:47:46 -0500

    Further work on review create action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,4 +16,14 @@ class ReviewsController < ApplicationController
     @book = Book.find(params[:book_id])
     @review = Review.new
   end
+  
+  def create
+    @review = Review.new(summary: params[:review][:summary], rating: params[:review][:rating], body: params[:review][:rating], book_id: params[:book_id], user_id: current_user.id)
+    @book = Book.find(params[:book_id])
+    if @review.save
+      redirect_to book_reviews_path(@book)
+    else
+      redirect_to book_path(@book)
+    end
+  end
 end
