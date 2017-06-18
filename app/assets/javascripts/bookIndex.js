5840b754c173a089d2867f6f36d64b71984f941e
Ray Mulligan <rmullig2@optonline.net>
2017-06-17 20:10:34 -0400

    Refactor books index

---
+++ app/assets/javascripts/bookIndex.js
@@ -0,0 +1,23 @@
+$(function () {
+  var books = $.ajax( { type: "GET", async: false, url: "/books/list" }).responseJSON;
+
+  function Book(book_info) {
+    let keys = Object.keys(book_info)
+    for (let i = 0; i < keys.length; i++) {
+      this[keys[i]] = book_info[keys[i]]
+    }
+  }
+
+  Book.prototype.appendToElement = function(element) {
+                                    $(element).append("<tr><td><a href=/books/" + this.id + ">" +this.title +
+                                                      " </a></td><td>" + this.author + "</td><td>" + this.year +
+                                                      "</td><td>" + fiction + "</td></tr>")
+  }
+  
+  books.forEach(function(book) {
+    var next_book = new Book(book);
+    
+    next_book.fiction ? fiction = "Yes" : fiction = "No";
+    next_book.appendToElement('tbody')
+  })
+})
